9b9d0be4c1c0d36424860ff7acb311a1c6498272
Shubhi Srivastava <119123562+ShubhiSrivastava09@users.noreply.github.com>
2024-05-10 14:33:44 +0530

    update main.py

--- Face_detection_Engine/main.py
+++ Face_detection_Engine/main.py
@@ -1,9 +1,20 @@
 import cv2
 def faceBox(faceNet,frame):
+    print(frame)
+    frameHeight=frame.shape[0]
+    frameWidth=frame.shape[1]
     blob=cv2.dnn.blobFromImage(frame,1.0,(227,227),[104,117,123],swapRB=False)
     faceNet.setInput(blob)
     detection=faceNet.forward()
-    print(detection)
+    bbox=[]
+    for i in range(detection.shape[2]):
+        confidence = detection[0,0,i,2]
+        if confidence>0.7:
+            x1=int(detection[0,0,i,3]*frameWidth)
+            y1=int(detection[0,0,i,4]*frameWidth)
+            x2=int(detection[0,0,i,5]*frameWidth)
+            y2=int(detection[0,0,i,6]*frameWidth)
+            bbox.append([x1,y1])
     return detection
 
 faceProto = "opencv_face_detector.pbtxt"
